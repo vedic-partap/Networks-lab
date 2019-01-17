68774f34dd2271942bf76c73a675224e2fc6f8e1
user <user@localhost.localdomain>
2019-01-17 16:08:52 +0530

    Add lab work of Assignment-3 (Get Ip from Host Name)

---
+++ ass-3/Lab/host_server.c
@@ -0,0 +1,172 @@
+/*
+			NETWORK PROGRAMMING WITH SOCKETS
+
+In this program we illustrate the use of Berkeley sockets for interprocess
+communication across the network. We show the communication between a server
+process and a client process.
+
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h> 
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include<stdio.h> //printf
+#include<string.h> //memset
+#include<stdlib.h> //for exit(0);
+#include<sys/socket.h>
+#include<errno.h> //For errno - the error number
+#include<netdb.h>	//hostent
+#include<arpa/inet.h>
+
+			/* THE SERVER PROCESS */
+
+int hostname_to_ip(char * hostname , char* ip)
+{
+	struct hostent *he;
+	struct in_addr **addr_list;
+	int i;
+		
+	if ( (he = gethostbyname( hostname ) ) == NULL) 
+	{
+		// get the host info
+		herror("gethostbyname");
+		return 1;
+	}
+
+	addr_list = (struct in_addr **) he->h_addr_list;
+	
+	for(i = 0; addr_list[i] != NULL; i++) 
+	{
+		//Return the first one;
+		strcat(ip , inet_ntoa(*addr_list[i]) );
+		strcat(ip,", ");
+		// return 0;
+	}
+	
+	return 1;
+}
+
+
+main()
+{
+	int			sockfd, newsockfd ; /* Socket descriptors */
+	int			clilen;
+	struct sockaddr_in	cli_addr, serv_addr;
+
+	int i;
+	char buf[100];		/* We will use this buffer for communication */
+
+	/* The following system call opens a socket. The first parameter
+	   indicates the family of the protocol to be followed. For internet
+	   protocols we use AF_INET. For TCP sockets the second parameter
+	   is SOCK_STREAM. The third parameter is set to 0 for user
+	   applications.
+	*/
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("Cannot create socket\n");
+		exit(0);
+	}
+
+	/* The structure "sockaddr_in" is defined in <netinet/in.h> for the
+	   internet family of protocols. This has three main fields. The
+ 	   field "sin_family" specifies the family and is therefore AF_INET
+	   for the internet family. The field "sin_addr" specifies the
+	   internet address of the server. This field is set to INADDR_ANY
+	   for machines having a single IP address. The field "sin_port"
+	   specifies the port number of the server.
+	*/
+	serv_addr.sin_family	= AF_INET;
+	inet_aton("127.0.0.1", &serv_addr.sin_addr);
+	serv_addr.sin_port	= htons(20000);
+
+
+	/* With the information provided in serv_addr, we associate the server
+	   with its port using the bind() system call. 
+	*/
+	if (bind(sockfd, (struct sockaddr *) &serv_addr,
+					sizeof(serv_addr)) < 0) {
+		printf("Unable to bind local address\n");
+		exit(0);
+	}
+
+	listen(sockfd, 5); /* This specifies that up to 5 concurrent client
+			      requests will be queued up while the system is
+			      executing the "accept" system call below.
+			   */
+
+	/* In this program we are illustrating a concurrent server -- one
+	   which forks to accept multiple client connections concurrently.
+	   As soon as the server accepts a connection from a client, it
+	   forks a child which communicates with the client, while the
+	   parent becomes free to accept a new connection. To facilitate
+	   this, the accept() system call returns a new socket descriptor
+	   which can be used by the child. The parent continues with the
+	   original socket descriptor.
+	*/
+	while (1) {
+
+		/* The accept() system call accepts a client connection.
+		   It blocks the server until a client request comes.
+
+		   The accept() system call fills up the client's details
+		   in a struct sockaddr which is passed as a parameter.
+		   The length of the structure is noted in clilen. Note
+		   that the new socket descriptor returned by the accept()
+		   system call is stored in "newsockfd".
+		*/
+		clilen = sizeof(cli_addr);
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr,
+					&clilen) ;
+
+		if (newsockfd < 0) {
+			printf("Accept error\n");
+			exit(0);
+		}
+
+		/* Having successfully accepted a client connection, the
+		   server now forks. The parent closes the new socket
+		   descriptor and loops back to accept the next connection.
+		*/
+		if (fork() == 0) {
+
+			/* This child process will now communicate with the
+			   client through the send() and recv() system calls.
+			*/
+			close(sockfd);	/* Close the old socket since all
+					   communications will be through
+					   the new socket.
+					*/
+
+			/* We initialize the buffer, copy the message to it,
+			   and send the message to the client. 
+			*/
+			
+			// strcpy(buf,"Message from server");
+			// send(newsockfd, buf, strlen(buf) + 1, 0);
+
+			/* We again initialize the buffer, and receive a 
+			   message from the client. 
+			*/
+			for(i=0; i < 100; i++) buf[i] = '\0';
+			recv(newsockfd, buf, 100, 0);
+			printf("%s\n", buf);
+			char ip[1000];
+			
+			hostname_to_ip(buf , ip);
+			printf("%s resolved to %s" , buf , ip);
+			
+			printf("\n");
+			close(newsockfd);
+			exit(0);
+		}
+
+		close(newsockfd);
+	}
+}
+			
+
